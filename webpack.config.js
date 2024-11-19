8c9d9980d86960ef05f3693548e1490c60075711
himacharan@123 <charanhima13@gmail.com>
2024-11-19 12:25:16 +0530

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: './index.js', 
+  entry: "./index.js", 
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  }, 
-  mode: 'development', 
+    path: path.resolve(__dirname, "dist"), 
+    filename: "bundle.js", 
+  },
+  mode: "development", 
   module: {
     rules: [
       {
-        test: /\.css$/, 
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i, 
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin(), // Cleans the 'dist' folder before each build
+    new HtmlWebpackPlugin({
+      template: "./index.html", // Use your existing `index.html`
+      filename: "index.html", // Output HTML file in `dist`
+    }),
+  ],
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    port: 5501, 
+    static: "./dist", // Serve files from the `dist` folder
+    port: 8080, // Development server port
+    open: true, // Automatically opens the browser
   },
 };
